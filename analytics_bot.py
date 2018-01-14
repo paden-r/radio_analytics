76bf3e08b397959118bfd78c029366651ac5deea
prose <paden.rose@q2ebanking.com>
2018-01-14 12:02:12 -0600

    Another reorganization to attempt to make code more reusable and to only use objects when necessary instead of always

--- analytics_bot.py
+++ analytics_bot.py
@@ -6,19 +6,21 @@ import os
 from analytics_utilities.twitter_bot import TwitterBot as Tb
 from analytics_utilities.screen_scraper import ScreenScrapper as Ss
 from analytics_utilities.db_connector import PostgreSQL as Ps
+from analytics_utilities.stats_and_graph import StatsAndGraph as SaG
 
 
 class KROXAnalytics(object):
     def __init__(self):
-        self.bot = Tb()
-        self.scrapper = Ss("http://www.101x.com/broadcasthistory")
-        self.pickle_file_name = os.environ['PKL_FILE']
-        self.db = Ps()
+        self.bot = None
+        self.scrapper = None
+        self.pickle_file_name = None
+        self.db = None
+        self.stat_and_graph = None
         self.logger = None
         self.log_directory = os.environ['LOG_DIRECTORY']
         self.build_logger()
         self.broadcast_info = None
-        self.lastest_timestamp = None
+        self.latest_timestamp = None
         self.questionable_flag = None
 
     def build_logger(self):
@@ -46,28 +48,32 @@ class KROXAnalytics(object):
         log_message = '\t'.join(message)
         self.logger(log_message)
 
-    def start_up(self):
+    def start_up(self, broadcast_url):
         """
         Function that is called first and gets the newest data
         :return:
         """
+        self.bot = Tb()
+        self.scrapper = Ss(broadcast_url)
+        self.pickle_file_name = os.environ['PKL_FILE']
+        self.db = Ps()
         try:
             last_run = pickle.load(open(self.pickle_file_name, 'rb'))
         except (OSError, IOError):
             last_run = {}
 
         if not last_run:
-            self.lastest_timestamp, self.broadcast_info = self.scrapper.parse_broadcast_page()
+            self.latest_timestamp, self.broadcast_info = self.scrapper.parse_broadcast_page()
         else:
-            self.lastest_timestamp, self.broadcast_info = self.scrapper.parse_broadcast_page(
+            self.latest_timestamp, self.broadcast_info = self.scrapper.parse_broadcast_page(
                 time_threshold=last_run['time_stamp'])
-        if self.lastest_timestamp is None:
+        if self.latest_timestamp is None:
             self.log(['INFO', 'No new broadcast history', 'exiting'])
             exit(0)
         else:
             self.logger(self.broadcast_info)
-        self.log(['INFO', 'Latest timestamp', str(self.lastest_timestamp)])
-        last_run['time_stamp'] = self.lastest_timestamp
+        self.log(['INFO', 'Latest timestamp', str(self.latest_timestamp)])
+        last_run['time_stamp'] = self.latest_timestamp
         pickle.dump(last_run, open(self.pickle_file_name, 'wb'))
         self.log(['INFO', 'Got broadcast history'])
 
@@ -125,13 +131,13 @@ class KROXAnalytics(object):
             insert_data = (full_timestamp, data['artist'], data['title'])
             self.db.insert_into_broadcast_history(insert_data)
 
-    def get_broadcast_history(self):
+    def get_broadcast_history(self, url):
         """
         Function that runs the analytics bot
         :return:
         """
         self.log(['INFO', 'Wake UP', 'GRABABRUSHANDPUTONALITTLEMAKEUP'])
-        self.start_up()
+        self.start_up(broadcast_url=url)
         self.log(['INFO', 'Analyzing'])
         self.analyze_broadcast_history()
 
@@ -151,90 +157,54 @@ class KROXAnalytics(object):
         day worth of history and stores it in the DB for later use.
         :return:
         """
-        self.log(['INFO', 'Getting the broadcast history'])
+        self.log(['INFO', 'Getting the broadcast history from the DB'])
+        self.db = Ps()
+        self.stat_and_graph = SaG()
         success, error = self.db.connect_to_db()
         if not success:
             self.log(['ERROR', 'DB error', error])
             exit(-1)
 
         broadcast_data = self.db.get_broadcast_history()
-        daily_artist_count = {}
-        daily_artist_song_count = {}
-        song_hourly_count = {}
-        for row in broadcast_data:
-            if isinstance(row, tuple):
-                timestamp = row[0]
-                # make the artist and title sql safe first
-                artist = row[1].replace("'", "''")
-                title = row[2].replace("'", "''")
-
-                date, time = str(timestamp).split(' ')
-                # populating data dicts, doing this is steps so I don't get confused looking back at this
-                if date in daily_artist_song_count:
-                    date_dict = daily_artist_count[date]
-                    if artist in date_dict:
-                        date_dict[artist] += 1
-                    else:
-                        date_dict[artist] = 1
-                else:
-                    daily_artist_count[date] = {}
-                    daily_artist_count[date][artist] = 1
-
-                if date in daily_artist_song_count:
-                    date_dict = daily_artist_song_count[date]
-                    if artist in date_dict:
-                        song_dict = date_dict[artist]
-                        if title in song_dict:
-                            song_dict[title] += 1
-                        else:
-                            song_dict[title] = 1
-                    else:
-                        date_dict[artist] = {}
-                        date_dict[artist][title] = 1
-                else:
-                    daily_artist_song_count[date] = {}
-                    daily_artist_song_count[date][artist] = {}
-                    daily_artist_song_count[date][artist][title] = 1
-
-                # convert the time format to military and grab the hour
-                military_hour = datetime.datetime.strptime(time, '%I:%M%p').strftime('%H')
-                if date in song_hourly_count:
-                    if military_hour in song_hourly_count[date]:
-                        song_hourly_count[date][military_hour] += 1
-                    else:
-                        song_hourly_count[date][military_hour] = 1
-                else:
-                    song_hourly_count[date] = {}
-                    song_hourly_count[date][military_hour] = 1
-            else:
-                self.logger(['ERROR', 'Did not get the expected info from the DB', 'breaking'])
-                self.log(row)
-                exit(-1)
+        summary, details, hourly = self.stat_and_graph.organize_data(broadcast_data)
 
         self.log(['INFO', 'Finished getting broadcast history and have organized it', 'sending to db'])
-        self.db.insert_into_daily_summary(daily_artist_count)
+        self.db.insert_into_daily_summary(summary)
         self.log(['INFO', 'Inserted daily summary'])
-        self.db.insert_into_daily_details(daily_artist_song_count)
+        self.db.insert_into_daily_details(details)
         self.log(['INFO', 'Inserted daily details'])
-        self.db.insert_hourly_counts(song_hourly_count)
+        self.db.insert_hourly_counts(hourly)
         self.log(['INFO', 'Finished all db inserts', 'moving to clear the broadcast history table'])
         self.db.clear_broadcast_history()
         self.log(['INFO', 'End Of Day procedures have finished', 'shutting down'])
         self.db.disconnect()
 
+    def run_end_of_week_analytics(self):
+        """
+        Function that gathers a weeks worth of data and does some basic analytics
+        :return:
+        """
+        self.bot = Tb()
+        self.stat_and_graph = SaG()
+        self.db = Ps()
+
     def run_end_of_month_analytics(self):
         """
-        Function that gathers calcuates some basic analytics for the last month of broadcast history
+        Function that gathers calculates some basic analytics for the last month of broadcast history
         :return:
         """
         pass
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        analytics = KROXAnalytics()
-        analytics.get_broadcast_history()
-    elif sys.argv[1] in ['eod', "eom"]:
+    if sys.argv[1] in ['eod', "eom", 'url']:
+        if sys.argv[1] == 'url':
+            if len(sys.argv) == 3:
+                analytics = KROXAnalytics()
+                analytics.get_broadcast_history(url=sys.argv[2])
+            else:
+                print("Unknown parameters given")
+                exit(-1)
         if sys.argv[1] == 'eod':
             analytics = KROXAnalytics()
             analytics.run_end_of_day_procedures()
@@ -243,5 +213,6 @@ if __name__ == '__main__':
             analytics.run_end_of_month_analytics()
     else:
         print("Unknown parameters given")
+        exit(-1)
 
 
---
+++ analytics_utilities/stats_and_graph.py
@@ -0,0 +1,75 @@
+import matplotlib
+import datetime
+
+
+class StatsAndGraph(object):
+    """
+    Class that holds methods to calculate stats on data and graph things
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def organize_data(data, logger=None):
+        """
+        Function that organizes the raw data gathered from the DB
+        :param data: A dictionary of raw data {timestamp: {artist, title}}
+        :type data: dict
+        :param logger: A logger object
+        :return:
+        """
+        if logger is None:
+            logger = lambda x: print(x)
+        daily_artist_count = {}
+        daily_artist_song_count = {}
+        song_hourly_count = {}
+        for row in data:
+            if isinstance(row, tuple):
+                timestamp = row[0]
+                # make the artist and title sql safe first
+                artist = row[1].replace("'", "''")
+                title = row[2].replace("'", "''")
+
+                date, time = str(timestamp).split(' ')
+                # populating data dicts, doing this is steps so I don't get confused looking back at this
+                if date in daily_artist_song_count:
+                    date_dict = daily_artist_count[date]
+                    if artist in date_dict:
+                        date_dict[artist] += 1
+                    else:
+                        date_dict[artist] = 1
+                else:
+                    daily_artist_count[date] = {}
+                    daily_artist_count[date][artist] = 1
+
+                if date in daily_artist_song_count:
+                    date_dict = daily_artist_song_count[date]
+                    if artist in date_dict:
+                        song_dict = date_dict[artist]
+                        if title in song_dict:
+                            song_dict[title] += 1
+                        else:
+                            song_dict[title] = 1
+                    else:
+                        date_dict[artist] = {}
+                        date_dict[artist][title] = 1
+                else:
+                    daily_artist_song_count[date] = {}
+                    daily_artist_song_count[date][artist] = {}
+                    daily_artist_song_count[date][artist][title] = 1
+
+                # convert the time format to military and grab the hour
+                military_hour = datetime.datetime.strptime(time, '%I:%M%p').strftime('%H')
+                if date in song_hourly_count:
+                    if military_hour in song_hourly_count[date]:
+                        song_hourly_count[date][military_hour] += 1
+                    else:
+                        song_hourly_count[date][military_hour] = 1
+                else:
+                    song_hourly_count[date] = {}
+                    song_hourly_count[date][military_hour] = 1
+            else:
+                logger('ERROR: Did not get the expected info from the DB breaking')
+                logger(row)
+                exit(-1)
+        return daily_artist_count, daily_artist_song_count, song_hourly_count
